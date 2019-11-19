7031ba869d67b6a470642a768939842b64cc7091
ClaudiaRivas <42727618+ClaudiaRivas@users.noreply.github.com>
2019-11-19 15:35:21 -0600

    Footer

---
+++ js/actions.js
@@ -0,0 +1,53 @@
+function aparecelogin (){
+	event.preventDefault();
+	$("#login").animate({
+		'top':'0'
+	}, 500);
+}
+function desaparecelogin (){
+	event.preventDefault();
+	$("#login").animate({
+		'top':'-100'
+	}, 500);
+}
+function desapareceRegistro(){
+	$("#oscurecer").fadeOut();
+}
+function desapareceFormulario(){
+	$("#registrar").fadeOut(300,desapareceRegistro);
+}
+function mostrarFormulario(){
+	$("#registrar").fadeIn();
+	$("#oscurecer").click (desapareceFormulario);
+	$("#cerrarRegistro").click(desapareceFormulario)
+}
+function apareceRegistro(e){
+	e.preventDefault();
+	$("#oscurecer").fadeIn(400, mostrarFormulario);
+}
+function mostrarLoginYRegistro(){
+	$("#activarLogin").click(aparecelogin);
+	$("#cerrar").click(desaparecelogin);
+
+	$("#activarRegistrar").click(apareceRegistro);
+}
+$(document).ready (mostrarLoginYRegistro);
+
+/*  Funcion del Boton hacia Arriba*/
+$(document).ready(function(){
+	$('.up').hide();
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100 ){
+			$('.up').fadeIn('1000');
+		}else{
+			$('.up').fadeOut('1000');
+		}
+	});
+
+	$('.up').click(function(){
+		$('body, html').animate({
+			scrollTop: 0
+		},150);
+	});
+});
